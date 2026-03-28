0ec126cdc1b067d09c3a3d9eba14bf1affdb4f7f
Sebastian Cieślik <sebastian.cieslik@microsoft.wsei.edu.pl>
2026-03-28 20:50:17 +0100

    dodawanie kodu

--- Bank/Bank.App/Program.cs
+++ Bank/Bank.App/Program.cs
@@ -1,1 +1,6 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Bank;
+Konto molenda = new Konto("Molenda", 100);
+Console.WriteLine($"Konto: założone. Klient: {molenda.Nazwa}, Bilans: {molenda.Bilans}");
+
+molenda.Wplata(50);
+Console.WriteLine($"Po wpłacie bilans wynosi: {molenda.Bilans}");
