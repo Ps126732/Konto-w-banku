b268396da3db97d1b9a0f2262723b3931d2d0b6c
Sebastian Cieślik <sebastian.cieslik@microsoft.wsei.edu.pl>
2026-03-30 14:37:20 +0200

    tworzenie klasy KontoLimit + tworzenie konstruktora

--- Bank/Bank/KontoLimit.cs
+++ Bank/Bank/KontoLimit.cs
@@ -1,10 +1,109 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Bank
 {
-    internal class KontoLimit
+    public class KontoLimit
     {
+      
+        private Konto konto;
+
+        private decimal limit;
+        private decimal wykorzystanyDebet = 0;
+
+        public decimal Limit
+        {
+            get { return limit; }
+            set { limit = value; }
+        }
+
+      
+        public string Nazwa => konto.Nazwa;
+        public bool Zablokowane => konto.Zablokowane;
+
+      
+        public decimal Bilans
+        {
+            get
+            {
+                if (Zablokowane) return 0;
+                return konto.Bilans + limit;
+            }
+        }
+
+       
+        public KontoLimit(string klient, decimal bilansNaStart = 0, decimal limit = 100)
+        {
+            this.konto = new Konto(klient, bilansNaStart);
+            this.limit = limit;
+        }
+
+        public void Wplata(decimal kwota)
+        {
+            if (kwota <= 0)
+                throw new ArgumentException("Kwota wpłaty musi być dodatnia.");
+
+            if (Zablokowane && wykorzystanyDebet > 0)
+            {
+                if (kwota >= wykorzystanyDebet)
+                {
+                    decimal reszta = kwota - wykorzystanyDebet;
+                    wykorzystanyDebet = 0;
+                    OdblokujKonto(); 
+
+                    if (reszta > 0)
+                    {
+                        konto.Wplata(reszta); 
+                    }
+                }
+                else
+                {
+                    wykorzystanyDebet -= kwota;
+                }
+            }
+            else
+            {
+                konto.Wplata(kwota); 
+            }
+        }
+
+        public void Wyplata(decimal kwota)
+        {
+            if (Zablokowane)
+                throw new InvalidOperationException("Konto jest zablokowane.");
+            if (kwota <= 0)
+                throw new ArgumentException("Kwota wypłaty musi być dodatnia.");
+
+            if (kwota <= konto.Bilans)
+            {
+               
+                konto.Wyplata(kwota);
+            }
+            else
+            {
+     
+                decimal brakujacaKwota = kwota - konto.Bilans;
+
+                if (brakujacaKwota <= limit)
+                {
+                  
+                    if (konto.Bilans > 0)
+                    {
+                        konto.Wyplata(konto.Bilans);
+                    }
+
+                    wykorzystanyDebet = brakujacaKwota;
+                    BlokujKonto(); 
+                }
+                else
+                {
+                    throw new InvalidOperationException("Przekroczono dostępny jednorazowy limit debetowy.");
+                }
+            }
+        }
+
+      
+        public void BlokujKonto() => konto.BlokujKonto();
+        public void OdblokujKonto() => konto.OdblokujKonto();
     }
 }
