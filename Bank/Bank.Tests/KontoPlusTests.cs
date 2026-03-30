fda7fe19c3ec4d03525b346bcef572aaec6c97e7
Sebastian Cieślik <sebastian.cieslik@microsoft.wsei.edu.pl>
2026-03-30 17:07:08 +0200

    poprawa KontoPlusTests.cs

--- Bank/Bank.Tests/KontoPlusTests.cs
+++ Bank/Bank.Tests/KontoPlusTests.cs
@@ -1,74 +1,54 @@
 ﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting; 
+using Xunit;
 using Bank;
 
 namespace Bank.Tests
 {
-    [TestClass] 
     public class KontoPlusTests
     {
-        [TestMethod] 
+        [Fact]
         public void Bilans_ZwracaSrodkiDoDyspozycji()
         {
-            
             KontoPlus konto = new KontoPlus("Jan Kowalski", 100, 200);
-
-           
-            Assert.AreEqual(300, konto.Bilans);
+            Assert.Equal(300m, konto.Bilans);
         }
 
-        [TestMethod] 
+        [Fact]
         public void Wyplata_ZWykorzystaniemDebetu_BlokujeKonto()
         {
-            
             KontoPlus konto = new KontoPlus("Jan Kowalski", 100, 200);
+            konto.Wyplata(250);
 
-           
-            konto.Wyplata(250); 
-
-            
-            Assert.IsTrue(konto.Zablokowane, "Konto powinno być zablokowane po jednorazowym debecie.");
-            Assert.AreEqual(0, konto.Bilans, "Po zablokowaniu bilans do dyspozycji powinien wynosić 0.");
+            Assert.True(konto.Zablokowane);
+            Assert.Equal(0m, konto.Bilans);
         }
 
-        [TestMethod] 
+        [Fact]
         public void Wyplata_PrzekroczenieLimitu_RzucaWyjatek()
         {
-            
             KontoPlus konto = new KontoPlus("Jan Kowalski", 100, 50);
-
-            
-            Assert.ThrowsException<InvalidOperationException>(() => konto.Wyplata(200));
+            Assert.Throws<InvalidOperationException>(() => konto.Wyplata(200));
         }
 
-        [TestMethod] 
+        [Fact]
         public void Wplata_CzesciowaSplataDebetu_NieOdblokowujeKonta()
         {
-            
             KontoPlus konto = new KontoPlus("Jan Kowalski", 0, 100);
-            konto.Wyplata(100); 
+            konto.Wyplata(100);
+            konto.Wplata(50);
 
-            
-            konto.Wplata(50); 
-
-            
-            Assert.IsTrue(konto.Zablokowane, "Konto powinno pozostać zablokowane przy niepełnej spłacie.");
+            Assert.True(konto.Zablokowane);
         }
 
-        [TestMethod] 
+        [Fact]
         public void Wplata_CalkowitaSplataDebetu_OdblokowujeKontoOrazPrzywracaLimit()
         {
-            
             KontoPlus konto = new KontoPlus("Jan Kowalski", 50, 100);
-            konto.Wyplata(100); 
+            konto.Wyplata(100);
+            konto.Wplata(60);
 
-           
-            konto.Wplata(60); 
-
-           
-            Assert.IsFalse(konto.Zablokowane, "Konto powinno się odblokować po całkowitej spłacie.");
-          
-            Assert.AreEqual(110, konto.Bilans);
+            Assert.False(konto.Zablokowane);
+            Assert.Equal(110m, konto.Bilans);
         }
     }
 }
